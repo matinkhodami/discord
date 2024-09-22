93baea0957daec590f05871a3459a73111b01b1b
mtnPhantom <mtnphantomwork@gmail.com>
2024-09-22 12:57:15 +0330

    change type!

--- src/components/server/lib/getServer.ts
+++ src/components/server/lib/getServer.ts
@@ -1,49 +1,92 @@
-import { db } from "@/lib/db"
-import { Server, Channel, Profile, MemberRole, Member } from "@prisma/client"
+import { db } from "@/lib/db";
+import { Server, MemberRole, ChannelType } from "@prisma/client";
 
 export type ServerInfo = Server & {
-    members: Member[]
-    channels: Channel[]
-}
+  members: {
+    id: string;
+    role: MemberRole;
+    profileID: string;
+    serverID: string;
+    createdAt: Date;
+    updatedAt: Date;
+    profile: {
+      id: string;
+      name: string;
+      image: string | null;
+      email: string;
+    };
+  }[];
+  channels: ({
+    profile: {
+      name: string;
+      id: string;
+      image: string | null;
+      email: string;
+    };
+  } & {
+    id: string;
+    name: string;
+    type: ChannelType;
+    serverID: string;
+    profileID: string;
+    createdAt: Date;
+    updatedAt: Date;
+  })[];
+};
 
-const getServerInfo = async (serverID: string): Promise<ServerInfo | null> => { 
-    const server = await db.server.findUnique({
-        where: {
-            id: serverID
+const getServerInfo = async (serverID: string): Promise<ServerInfo | null> => {
+  const server = await db.server.findUnique({
+    where: {
+      id: serverID,
+    },
+    include: {
+      members: {
+        include: {
+          profile: {
+            select: {
+              email: true,
+              id: true,
+              image: true,
+              name: true,
+            },
+          },
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+      channels: {
+        orderBy: {
+          createdAt: "asc",
         },
         include: {
-            members: {
-                include: {
-                    profile: true
-                },
-                orderBy: {
-                    role: "asc"
-                }
+          server: false,
+          profile: {
+            select: {
+              email: true,
+              id: true,
+              image: true,
+              name: true,
             },
-            channels: {
-                orderBy: {
-                    createdAt: "asc"
-                },
-                include: {
-                    profile: true
-                }
-            }
-        }
-    })
-    return server as ServerInfo | null
-}
+          },
+        },
+      },
+    },
+  });
+  return server || null;
+};
 
 const getServers = async (userID: string): Promise<Server[]> => {
-    const servers = await db.server.findMany({
-        where: {
-            members: {
-                some: {
-                    profileID: userID,
-                }
-            }
-        }
-    })
-    return servers
-}
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileID: userID,
+        },
+      },
+    },
+  });
+  return servers;
+};
 
-export { getServers, getServerInfo }
+export { getServers, getServerInfo };
